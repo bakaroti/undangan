e36c4c39420ac38c1bf567f51e4ed8ae21429179
bakaroti <dafajibran00@gmail.com>
2023-07-13 00:12:56 -0700

    update 13

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('bukaUndanganButton').addEventListener('click', function(e) {
-        e.preventDefault();
-        var targetElement = document.getElementById(this.getAttribute('href').substring(1));
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
-        });
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
     });
 });
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
+
+
+function countdown() {
+  const weddingDate = new Date("2032-03-03T08:00:00Z"); // Tanggal pernikahan
+  const now = new Date();
+
+  const diff = weddingDate - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+}
+
+setInterval(countdown, 1000);
